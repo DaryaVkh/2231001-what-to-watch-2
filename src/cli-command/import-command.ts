1b15a82aba273ccdfd50b63f707b68a6b615cce7
Darya <dashav1605@mail.ru>
2022-10-09 15:37:39 +0500

    Приложение для терминала. Часть 2

--- src/cli-command/import-command.ts
+++ src/cli-command/import-command.ts
@@ -5,7 +5,7 @@ import {createMovie, getErrorMessage} from '../utils/common-functions.js';
 export default class ImportCommand implements CliCommandInterface {
   readonly name = '--import';
 
-  public async execute(filename: string): Promise<void> {
+  async execute(filename: string): Promise<void> {
     const fileReader = new TSVFileReader(filename.trim());
     fileReader.on('line', (line) => console.log(createMovie(line)));
     fileReader.on('end', (count) => console.log(`${count} rows imported.`));
